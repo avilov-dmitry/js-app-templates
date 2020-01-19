4bbb65004cd6bf0c6403482004af8af3b4890104
Дмитрий Авилов <avilov.d.i@yandex.ru>
2020-01-19 23:51:16 +0300

    add simple-react app template

---
+++ simple-react/README.md
@@ -0,0 +1,17 @@
+Инициализация проекта:
+npm init
+
+Установка DEV зависимостей:
+npm i -D webpack webpack-cli webpack-dev-server babel-loader @babel/core @babel/preset-env @babel/preset-react style-loader css-loader html-webpack-plugin
+
+Установка общих зависимостей:
+npm i react react-dom
+
+Добавить .babelrc - в нем указываем какие preset'ы использовать
+
+Добавляем .gitignore
+
+Добавляем скрипты для develop и production мода в package.json
+
+Добавляем webpack.config.js
+
---
+++ simple-react/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "/dist")
+    },
+    module: {
+        rules: [
+            {
+                use: {
+                    loader: "babel-loader"
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                use: ["style-loader", "css-loader"],
+                test: /\.css$/
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+};
